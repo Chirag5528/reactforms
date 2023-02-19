0a26ab006a1f0a45964f88d748a11bc9d5625837
Chirag Arora <c.arora@contentbloom.com>
2023-02-19 20:11:19 +0530

    feat: Added AWS Authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactforms8e0c00f0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
 function App() {
   return (
-    <>
-      <h1> This is something that should come</h1>
-    </>
+    <React.Fragment>
+      <View className="App">
+        <Card>
+          <Image src={logo} className="App-logo" alt="logo" />
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
